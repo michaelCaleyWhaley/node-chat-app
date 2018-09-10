ac134073e60402e0aff52d0852035620ef47c89d
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2018-09-10 16:43:56 +0100

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,8 +13,19 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-
     console.log('New user connected');
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app.',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined.',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('createMessage', (createdMessage) => {
         io.emit('newMessage', {
