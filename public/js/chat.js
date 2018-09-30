1875b5a1a447ff626d4c049d6aca94e636a7e0a6
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2018-09-30 19:39:03 +0100

    adding in name and room functionality

--- public/js/chat.js
+++ public/js/chat.js
@@ -21,7 +21,21 @@ function scrollToBottom() {
 }
 
 socket.on('connect', function () {
-    console.log('Connected to server');
+    
+    var params = window.deparam(window.location.search);
+
+    socket.emit('join', params, function(err){
+        if(err){
+            alert(err);
+            window.location.href = '/';
+        } else {
+            console.log('No error');
+        }
+    });
+
+
+
+
 });
 
 // prints message to screen
